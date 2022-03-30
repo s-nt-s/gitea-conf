ff2b6685b24a4f212504d5b336c1478d4f5f7125
s-nt-s <santos82@gmail.com>
2022-03-30 14:30:29 +0200

    Mejorar trazas

--- core/git.py
+++ core/git.py
@@ -99,7 +99,7 @@ class Gitea:
         if kwargs.get('data') and isinstance(kwargs['data'], dict):
             kwargs['data'] = json.dumps(kwargs['data'])
         if log:
-            curl = "curl -X {} '{}'".format(verb, url)
+            curl = f"curl -X {verb} '{url}'"
             if kwargs.get('data'):
                 curl = curl + " -d '" + kwargs['data'] + "'"
             logger.log(log, "$ " + curl)
@@ -111,8 +111,14 @@ class Gitea:
             time.sleep(2)
         return safe_json(self.last_response)
 
-    def get(self, *args, **kwargs):
-        return self.rqs('GET', *args, **kwargs)
+    def get(self, *args, null404=False, **kwargs):
+        """
+        :param null404: Si es True y la petición da un 404 se devuelve None
+        """
+        r = self.rqs('GET', *args, **kwargs)
+        if null404 and self.last_response.status_code == 404:
+            return None
+        return r
 
     def post(self, *args, **kwargs):
         return self.rqs('POST', *args, **kwargs)
@@ -128,7 +134,7 @@ class Gitea:
         count = -1
         while True:
             count = count + 1
-            page = self.rqs('GET', url+"?page="+str(count), *args, **kwargs)
+            page = self.rqs('GET', url + "?page=" + str(count), *args, **kwargs)
             rt.extend(page)
             if len(page) == 0 or str(len(rt)) == self.last_response.headers['X-Total-Count']:
                 return rt
@@ -144,32 +150,32 @@ class Gitea:
             - fail: fallar si existe
         :return: Información de la organización
         """
-        logger.debug("# Verificar si existe la organización %s", org)
-        r = self.get('/orgs/{}'.format(org))
-        if r.get('message') == 'Not Found':
-            logger.info("# Crear organización %s", org)
+        logger.debug(f"# Verificar si existe la organización {org}")
+        r = self.get(f'/orgs/{org}', null404=True)
+        if r is None:
+            logger.info(f"# Crear organización {org}")
         else:
             if if_exists == 'fail':
-                raise ExAlreadyExist("org {} ya existe".format(org))
+                raise ExAlreadyExist(f"org {org} ya existe")
             if if_exists == 'reuse':
-                logger.info("# Reusar organización %s", org)
+                logger.info(f"# Reusar organización {org}")
                 return r
             if if_exists == 'overwrite':
-                logger.info("# Recrear organización %s", org)
+                logger.info(f"# Recrear organización {org}")
                 # TODO: Implementar borrar organización con cuidado de no borrar usuarios
                 raise Exception("overwrite no implementado aún")
             else:
-                raise Exception("valor inadecuado para if_exists: %s" % if_exists)
+                raise Exception(f"valor inadecuado para if_exists: {if_exists}")
 
         cfg_org = read_js("config/org.json")
         cfg_org['full_name'] = org
         cfg_org['username'] = org
         r = self.post('/orgs', data=cfg_org)
         if r.get('message', '').startswith('user already exists'):
-            raise ExOrgIsUser("No se puede crear la organización {} porque es un usuario".format(org))
+            raise ExOrgIsUser(f"No se puede crear la organización {org} porque es un usuario")
         if not (isinstance(r, dict) and "id" in r):
             raise Exception("Salida no esperada %s" % r)
-        r = self.get('/orgs/{}'.format(org))
+        r = self.get(f'/orgs/{org}')
         return r
 
     def new_repo(self, org, repo, if_exists='fail'):
@@ -183,31 +189,28 @@ class Gitea:
             - fail: fallar si existe
         :return: Información del repositorio
         """
-        logger.debug("# Verificar si existe el repositorio %s/%s", org, repo)
-        r = self.get('/repos/{}/{}'.format(org, repo))
-        if r.get('message') == 'Not Found':
-            logger.info("# Crear repositorio %s/%s", org, repo)
+        logger.debug(f"# Verificar si existe el repositorio {org}/{repo}")
+        r = self.get(f'/repos/{org}/{repo}', null404=True)
+        if r is None:
+            logger.info(f"# Crear repositorio {org}/{repo}")
         else:
             if if_exists == 'fail':
-                raise ExAlreadyExist("repo {} ya existe".format(org))
+                raise ExAlreadyExist(f"repo {org}/{repo} ya existe")
             if if_exists == 'reuse':
-                logger.info("# Reusar repositorio %s/%s", org, repo)
+                logger.info(f"# Reusar repositorio {org}/{repo}")
                 return r
             if if_exists == 'overwrite':
-                logger.info("# Recrear repositorio %s/%s", org, repo)
-                self.delete('/repos/{}/{}'.format(org, repo))
+                logger.info(f"# Recrear repositorio {org}/{repo}")
+                self.delete(f'/repos/{org}/{repo}')
             else:
-                raise Exception("valor inadecuado para if_exists: %s" % if_exists)
+                raise Exception(f"valor inadecuado para if_exists: {if_exists}")
 
         cfg_repo = read_js("config/repo.json")
         cfg_repo['name'] = repo
-        r = self.post(
-            '/orgs/{}/repos'.format(org),
-            data=cfg_repo
-        )
+        r = self.post(f'/orgs/{org}/repos', data=cfg_repo)
         if not (isinstance(r, dict) and "id" in r):
             raise Exception("Salida no esperada %s" % r)
-        r = self.get('/repos/{}/{}'.format(org, repo))
+        r = self.get(f'/repos/{org}/{repo}')
         return r
 
     def get_repos(self, org, repo) -> list:
@@ -217,20 +220,18 @@ class Gitea:
         :param repo: Si es '' obtiene todos los repositorios de la organización
         """
         if "" not in (org, repo):
-            r = self.get('/repos/{}/{}'.format(org, repo))
-            if r.get('message') == 'Not Found':
-                return []
-            return [r]
+            r = self.get(f'/repos/{org}/{repo}', null404=True)
+            return [r] if r else []
         orgs = [org]
         if org == "":
             orgs = [i["username"] for i in self.get_list('/orgs')]
         repos = []
         for org in orgs:
             if repo == "":
-                repos.extend(self.get_list("/orgs/{}/repos".format(org)))
+                repos.extend(self.get_list(f"/orgs/{org}/repos"))
             else:
-                r = self.get('/repos/{}/{}'.format(org, repo))
-                if r.get('message') == 'Not Found':
+                r = self.get(f'/repos/{org}/{repo}', null404=True)
+                if r:
                     repos.append(r)
         return repos
 
@@ -240,29 +241,28 @@ class Gitea:
         :param org: Nombre de la organización
         :param repo: Nombre del repositorio
         """
-        logger.debug("# Verificar si el repositorio %s es un usuario o una organización", org)
-        r = self.get('/orgs/{}'.format(org))
-        if r.get('message') == 'Not Found':
+        logger.debug(f"# Verificar si el repositorio {org} es un usuario o una organización")
+        if self.get(f'/orgs/{org}', null404=True) is None:
             # Es un repositorio de usuario, no de una organización
             # TODO: ¿Qué hacer con los repositorios de usuarios?
-            raise ExRepoIsUser("{}/{} es un repositorio de usuario".format(org, repo))
-        logger.debug("# Verificar si existe el repositorio %s/%s", org, repo)
-        r = self.get('/repos/{}/{}'.format(org, repo))
-        if r.get('message') == 'Not Found':
-            raise ExNotFound("{}/{} no existe".format(org, repo))
+            raise ExRepoIsUser(f"{org}/{repo} es un repositorio de usuario")
+        logger.debug(f"# Verificar si existe el repositorio {org}/{repo}")
+        r = self.get(f'/repos/{org}/{repo}', null404=True)
+        if r is None:
+            raise ExNotFound(f"{org}/{repo} no existe")
 
         # Comprobar que la rama por defecto debe ser la definida en config/repo.json
         default_config = read_js("config/repo.json")
         mustdef_branch = default_config['default_branch']
         default_branch = r['default_branch']
         if default_branch != mustdef_branch:
-            logger.info("# Corregir rama por defecto: %s/%s %s -> %s", org, repo, default_branch, mustdef_branch)
-            self.patch("/repos/{}/{}".format(org, repo), data={"default_branch": mustdef_branch})
+            logger.info(f"# Corregir rama por defecto: {org}/{repo} {default_branch} -> {mustdef_branch}")
+            self.patch(f"/repos/{org}/{repo}", data={"default_branch": mustdef_branch})
             default_branch = mustdef_branch
 
-        logger.debug("# Verificar si existe la rama %s/%s %s", org, repo, default_branch)
-        br = self.get("/repos/{}/{}/branches/{}".format(org, repo, default_branch))
-        if isinstance(br, dict) and br.get('errors') and len(br['errors']) > 0:
+        logger.debug(f"# Verificar si existe la rama {org}/{repo} {default_branch}")
+        br = self.get(f"/repos/{org}/{repo}/branches/{default_branch}")
+        if self.last_response.status_code == 404:
             # TODO: Evitar error inicializando el repositorio
             # https://github.com/go-gitea/gitea/issues/10993
             # https://github.com/go-gitea/gitea/issues/10982
@@ -285,30 +285,59 @@ class Gitea:
             for k, v in limits.items():
                 if k.endswith("_teams") and v:
                     teams = teams.union(v)
-        logger.debug("# Obtener grupos de %s", org)
-        teams = sorted(teams - set(t['name'] for t in self.get('/orgs/{}/teams'.format(org))))
+        teams = sorted(teams)
         branches = sorted(branches)
 
         for branch in branches:
-            logger.debug("# Verificar si existe la rama %s/%s %s", org, repo, branch)
-            br = self.get("/repos/{}/{}/branches/{}".format(org, repo, branch))
-            if isinstance(br, dict) and br.get('errors') and len(br['errors']) > 0:
-                logger.info("# Crear rama %s/%s %s", org, repo, branch)
+            logger.debug(f"# Verificar si existe la rama {org}/{repo} {branch}")
+            if self.get(f"/repos/{org}/{repo}/branches/{branch}", null404=True) is None:
+                logger.info(f"# Crear rama {org}/{repo} {branch}")
                 # Si no existe la rama, se crea a partir de default_branch
                 self.post(
-                    '/repos/{}/{}/branches'.format(org, repo),
+                    f'/repos/{org}/{repo}/branches',
                     data={
                         "new_branch_name": branch,
                         "old_branch_name": default_branch
                     }
                 )
 
-        for team in teams:
-            logger.info("# Crear grupo %s %s", org, team)
-            data = read_js("config/teams/{}.json".format(team.lower())) or read_js("config/teams/default.json")
-            data['name'] = team
-            self.post('/orgs/{}/teams'.format(org), data=data)
+        logger.info(f"# Crear grupos {org}: {', '.join(teams)}")
+        self.create_teams(org, *teams)
 
-        logger.info("# Aplicar permisos en %s/%s: %s", org, repo, ", ".join(branches))
-        for limits in all_limits:
-            self.post('/repos/{}/{}/branch_protections'.format(org, repo), data=limits)
+        logger.info(f"# Aplicar permisos en {org}/{repo}: {', '.join(branches)}")
+        self.branch_protections(org, repo, *all_limits)
+
+    def create_teams(self, org, *teams):
+        """
+        Crea el grupo team en la organización org
+        Si el grupo ya existe, actualiza su configuración
+        """
+        logger.debug("# Obtener grupos de %s", org)
+        old_teams = {t['name']: t for t in self.get_list(f'/orgs/{org}/teams')}
+
+        for team in teams:
+            confg = read_js(f"config/teams/{team.lower()}.json") or read_js("config/teams/default.json")
+            confg['name'] = team
+            if team in old_teams:
+                logger.debug(f"# Reconfigurar grupo {org}: {team}")
+                self.patch(f"/teams/{old_teams[team]['id']}", data=confg)
+            else:
+                logger.debug(f"# Crear grupo {org}: {team}")
+                self.post(f'/orgs/{org}/teams', data=confg)
+
+    def branch_protections(self, org, repo, *limits_list):
+        """
+        Protege una rama limits['branch_name'] del repositorio org/repo con los límites limits
+        Si ya existía una protección, la elimina y la vuelve a crear de cero
+        """
+        for limits in limits_list:
+            branch = limits['branch_name']
+            logger.debug(f"# Verificar si existe la protección para la rama {org}/{repo} {branch}")
+            if self.get(f'/repos/{org}/{repo}/branch_protections/{branch}', null404=True):
+                logger.debug(f"# Crear protección para la rama {org}/{repo} {branch}")
+                self.post(f'/repos/{org}/{repo}/branch_protections/', data=limits)
+            else:
+                logger.debug(f"# Borrar protección para la rama {org}/{repo} {branch}")
+                self.delete(f'/repos/{org}/{repo}/branch_protections/')
+                logger.debug(f"# Recrear protección para la rama {org}/{repo} {branch}")
+                self.patch(f'/repos/{org}/{repo}/branch_protections/{branch}', data=limits)
